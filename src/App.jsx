d3b220220eda471e797972756129d76fde4f6b8c
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-22 14:24:50 -0800

    update basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header'; 
+import Footer from './components/Footer/Footer';
 import './App.scss';
 
 function App() {
     return(
     <Router>
     <Header />
+    <Footer />
   </Router>
     )
 
