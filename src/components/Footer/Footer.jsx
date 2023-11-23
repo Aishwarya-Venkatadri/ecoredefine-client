fa23724de80bd702bdd5a6a214f97af9410d87c2
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-22 21:56:55 -0800

    adds footer component with styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import '../Footer/Footer.scss'
 import fb from '../../assets/Icons/fb.svg'
 import insta from '../../assets/Icons/insta.svg'
 import tweet from '../../assets/Icons/tweet.svg'
+
 
 
 function Footer(){
@@ -10,7 +12,7 @@ function Footer(){
        <div className="footer">
         <div className="footer-icons">
             <p className="footer-icons__para">
-            Follow us on our socials
+            Our socials
             </p>
             <div className="footer-icons__iconlist">
             <a   href="https://www.facebook.com/"><img className="footer-icons--stylemod" src={fb} alt="facebook icon" /></a>
@@ -19,7 +21,12 @@ function Footer(){
         </div>
         </div>
         <div className="footer-redirects">
-            <ul>More</ul>
+            <ul>Resources
+            <Link to="/blogs"><li>Blogs</li></Link>
+            <Link to="/contact"><li>Contact us</li></Link>
+            <Link to="/suggestion"><li>Suggestion Box</li></Link>
+            <Link to="/privacy"><li>Privacy</li></Link>
+            </ul>
 
         </div>
        </div>
