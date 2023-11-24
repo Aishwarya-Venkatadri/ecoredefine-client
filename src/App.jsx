55e82c01de0162bdd69c79afaad63c89120b3985
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-23 21:53:10 -0800

    🍱 adds assets for the hero image

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header'; 
 import Footer from './components/Footer/Footer';
+import Hero from './components/Hero.jsx/Hero';
 import './App.scss';
 
 function App() {
     return(
     <Router>
     <Header />
-   
+   <Hero />
     <Footer />
   </Router>
     )
---
+++ src/components/Hero.jsx/Hero.jsx
@@ -0,0 +1,13 @@
+
+import '../Hero.jsx/Hero.scss'
+
+function Hero(){
+    return(
+        <div className="hero">
+            <div class="hero__bgwrapper"></div>
+            <h2 class="hero__title">EcoRedefine - "Reducing Waste, Redefining Ownership</h2>
+        </div>
+    )
+}
+
+export default Hero;
