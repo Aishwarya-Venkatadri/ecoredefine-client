4e138a5bd393cde702528bfafded0f79f3f386a6
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-28 09:24:37 -0800

    changes to the category component and the associated scss

--- src/pages/CategoryItemPage/CategoryItemPage.jsx
+++ src/pages/CategoryItemPage/CategoryItemPage.jsx
@@ -1,19 +1,24 @@
+// CategoryItemPage.jsx
+
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import './CategoryItemPage.scss';
 
 function CategoryItemPage() {
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [categories, setCategories]= useState([])
+  console.log(categories);
   const { id } = useParams();
-  console.log(id);
 
   useEffect(() => {
     async function getListingsData() {
       try {
         const response = await axios.get(`http://localhost:5050/categories/${id}`);
-        console.log('Listings Data:', response.data);
         setListings(response.data.listings);
+        setCategories(response.data.category);
       } catch (error) {
         console.error('Error fetching listings:', error);
       }
@@ -22,21 +27,45 @@ function CategoryItemPage() {
     getListingsData();
   }, [id]);
 
-  console.log('Listings:', listings);
-
   return (
-    <div>
-      <h2>Category Item Page</h2>
-      <p>Category ID: {id}</p>
-      <ul className='listings-list'>
-        {listings.map((listing) => (
-          <li key={listing.listing_id}>
-           <img src={`http://localhost:5050/listings-images/${listing.image}`} alt={`Listing ${listing.listing_id}`} />
+    <>
+      <Header />
+      <div>
+        <p>You are viewing: {categories.category_name}</p>
+        <div className="product-cards">
+          {listings.map((listing) => (
+            <div key={listing.listing_id} className="product-card el-wrapper">
+              <div className="box-up">
+                <img src={`http://localhost:5050/listings-images/${listing.image}`} alt={`Listing ${listing.listing_id}`} className="img" />
+                <div className="img-info">
+                  <div className="info-inner">
+                    <span className="p-name">{listing.listing_name}</span>
+                    <span className="p-company">{listing.category_name}</span>
+                  </div>
+                  <div className="a-size">Location: {listing.location}</div>
+                </div>
+              </div>
 
-          </li>
-        ))}
-      </ul>
-    </div>
+              <div className="box-down">
+                <div className="h-bg">
+                  <div className="h-bg-inner"></div>
+                </div>
+
+                <Link to={`/listings/${listing.listing_id}`} className="cart">
+                  <span className="price">${listing.listing_borrow_price}</span>
+                  <span className="add-to-cart">
+                    <span className="txt" disabled={listing.availability === 'Unavailable'}>
+                      {listing.availability === 'Unavailable' ? 'Out of Stock' : 'Borrow'}
+                    </span>
+                  </span>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
