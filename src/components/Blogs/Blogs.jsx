d9b7da87b1afae3c590237cab13c0555cf8ca8c5
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-30 01:07:18 -0800

    adds  the Blogs component and associated styles

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import '../Blogs/Blogs.scss'; // Import the corresponding SCSS file
+
+const BlogCard = ({ blog }) => {
+  return (
+    <div className="blog-card">
+      <img src={blog.image} alt={blog.title} />
+      <div className="card-summary">
+        <h3>{blog.title}</h3>
+        <p>{blog.summary}</p>
+        <Link to={`/blog/${blog.id}`}>Read More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
